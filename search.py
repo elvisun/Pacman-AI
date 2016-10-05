a2443406d559ae7a034ec377b2c34f4df066861e
Elvis Sun <elvissunlihe@gmail.com>
2016-10-04 23:19:53 -0400

    general A*

--- search.py
+++ search.py
@@ -253,57 +253,53 @@ def nullHeuristic(state, problem=None):
   """
   return 0
 
+
+####################A STAR DEFAULT######################
+# def aStarSearch(problem, heuristic=nullHeuristic):
+#   Q = util.PriorityQueue()
+#   explored = {}
+#   startingPosition = problem.getStartState()
+#   Q.push((startingPosition, []), heuristic(startingPosition, problem))
+
+#   while not Q.isEmpty():
+#     node, directions = Q.pop()
+#     #print node,directions
+#     if problem.isGoalState(node):
+#         return directions
+
+#     explored[node] = True
+#     for coord, direction, cost in problem.getSuccessors(node):
+#         if not coord in explored:
+#             newDirection = directions + [direction]
+#             score = problem.getCostOfActions(newDirection) + heuristic(coord, problem)
+#             Q.push((coord, newDirection), score)
+
+#   return []
+#   "*** YOUR CODE HERE ***"
+#   util.raiseNotDefined()
+
 def aStarSearch(problem, heuristic=nullHeuristic):
-  nextState = (problem.getStartState(),0,0)
+  Q = util.PriorityQueue()
   explored = {}
   route = []
-  print ''
-  while not problem.isGoalState(nextState[0]):
-    foundNext = False
-    frontier = problem.getSuccessors(nextState[0])
-    frontier = sorted(frontier, key = lambda k: heuristic(k[0],problem), reverse = False)
-    for each in frontier:
-      #print each
-      if not each[0] in explored:
-        route.append(nextState)
-        explored[each[0]] = True
-        nextState = each
-        foundNext = True
-        break
+  startingPosition = problem.getStartState()
+  Q.push((startingPosition, []), heuristic(startingPosition, problem))
 
-    if foundNext:
-      continue
+  while not Q.isEmpty():
+    node, directions = Q.pop()
+    #print node,directions
+    if problem.isGoalState(node):
+        return directions
 
-    if not len(route):
-      break
-    nextState = route.pop()
-    #stack.extend(problem.getSuccessors(nextState[0]))
+    explored[node] = True
+    for coord, direction, cost in problem.getSuccessors(node):
+        if not coord in explored:
+            newDirection = directions + [direction]
+            score = problem.getCostOfActions(newDirection) + heuristic(coord, problem)
+            Q.push((coord, newDirection), score)
 
-  print route
-  print len(route)
-  print nextState
-
-  route.append(nextState)
-  s = Directions.SOUTH
-  w = Directions.WEST
-  e = Directions.EAST
-  n = Directions.NORTH
-
-  returningList = []
-  for r in route[1:]:
-    direction = r[1]
-    #print direction
-    if direction == 'West':
-      returningList.append(w)
-    if direction == 'East':
-      returningList.append(e)
-    if direction == 'North':
-      returningList.append(n)
-    if direction == 'South':
-      returningList.append(s)
-  
-
-  return returningList
+  return []
+  "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
     
   
